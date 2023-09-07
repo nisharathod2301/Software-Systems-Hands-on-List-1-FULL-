39fbf824e14c0af269ce3a5dda0b797feb8ddf32
Nisha Hitesh Rathod <109743699+nisharathod2301@users.noreply.github.com>
2023-09-07 15:56:56 +0530

    Update 1b.c

--- 1b/1b.c
+++ 1b/1b.c
@@ -1,3 +1,11 @@
+/*
+============================================================================
+Name : 1b.c
+Author : Nisha Rathod
+Description : Create the following types of a files using (i) shell command (ii) system call b. hard link (link system call)
+Date: 5th Sept, 2023.
+============================================================================
+*/
 #include<stdio.h>
 #include<unistd.h>
 int main(){
